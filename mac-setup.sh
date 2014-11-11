15df4ccf1db864912b66bd33f8d62b544f75fa8a
Julio Arias <jarias01@gmail.com>
2014-11-11 11:59:01 -0600

    Install ansible using homebrew

--- mac-setup.sh
+++ mac-setup.sh
@@ -10,8 +10,7 @@ sudo sed -i .bak -e 's/^# \(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/\1/g' /etc/sudoers
 sudo dscl . append /Groups/wheel GroupMembership $USER
 
 #Install Ansible
-sudo easy_install pip
-sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
+brew install ansible
 
 #Run Ansible
 PYTHONIOENCODING='utf-8' ansible-playbook desktop.yml
