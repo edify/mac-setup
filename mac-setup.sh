fa799b93fff8075ffce19c5e8b2e8f3959b99e43
Julio Arias <julio.arias.edify.cr>
2014-11-14 12:05:47 -0600

    Fix sudoers file

--- mac-setup.sh
+++ mac-setup.sh
@@ -19,5 +19,5 @@ PYTHONIOENCODING='utf-8' ansible-playbook desktop.yml
 rm -rf ~/src
 
 #Return to normal password sudo
-sudo sed -i .bak -e 's/^\(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/#\1/g' /etc/sudoers
+sudo sed -i .bak -e 's/^\(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/# \1/g' /etc/sudoers
 sudo dscl . delete /Groups/wheel GroupMembership $USER
